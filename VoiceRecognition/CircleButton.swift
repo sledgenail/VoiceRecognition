fd0566febfae8c94eb4bb07d1f74affd79eca988
Emmanuel Erilibe <sledgenail@gmail.com>
2016-11-27 23:05:45 -0600

    Finished with bugs

---
+++ VoiceRecognition/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  VoiceRecognition
+//
+//  Created by Emmanuel Erilibe on 11/27/16.
+//  Copyright © 2016 Emmanuel Erilibe. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
